4a0e3e999ca6378692d0bec201ff66b3e0eb7bd7
josephiskingkong <iloveuyungcole@gmail.com>
2023-05-29 10:46:47 +0700

    Major update
    New design, disconnection, bug fix

--- public/script.js
+++ public/script.js
@@ -1,55 +1,88 @@
 let userId;
-let socket; 
+let socket;
 
 const input = document.getElementById('input');
 const sendButton = document.getElementById('send');
 const confirmUserIdButton = document.getElementById('confirm-user-id')
 const modalBackground = document.getElementById('modal-background');
+const error = document.getElementById('error');
 
+const sendMessage = async function () {
+    let message = input.value;
+    message = message.trimStart();
+    message = message.trimEnd();
+    if (message === '') {
+        return;
+    } 
+    await socket.emit('chat.message', message);
+    input.value = '';
+};
 
-confirmUserIdButton.onclick = function() {
+document.getElementById('user-id').onclick = function () {
+    error.classList.add('hidden');
+}
+
+function connect() {
     userId = document.getElementById('user-id').value;
+    userId = userId.trimStart();
+    if (userId === '' || userId.length > 20 || userId.length < 3) {
+        error.classList.remove('hidden');
+                return;
+    }
     socket = io();
     socket.emit('user.connected', userId);
-    
 
-    document.getElementById('send').onclick = function() {
-        const message = document.getElementById('input').value;
-        socket.emit('chat.message', { userId: userId, text: message });
-        document.getElementById('input').value = '';
+    sendButton.onclick = async function () {
+        sendMessage();
     };
 
-    socket.on('chat.message', function(message) {
-        if (message.userId !== userId) {
-        const userIdDiv = document.createElement('div');
-        userIdDiv.textContent = message.userId;
-        userIdDiv.className = 'userId';
-        document.getElementById('messages').appendChild(userIdDiv);
+    input.onkeydown = function (event) {
+        if (event.key === 'Enter') {
+            sendMessage();
         }
-    
+    };
+
+    socket.on('chat.message', function (message) {
+        if (message.userId !== userId) {
+            const userIdDiv = document.createElement('div');
+            userIdDiv.textContent = message.userId;
+            userIdDiv.className = 'userId';
+            document.getElementById('messages').appendChild(userIdDiv);
+        }
+
         const messageTextDiv = document.createElement('div');
         messageTextDiv.className = 'message ' + (message.userId === userId ? 'current-user' : 'other-user');
         messageTextDiv.textContent = message.text;
         document.getElementById('messages').appendChild(messageTextDiv);
     });
 
-    modalBackground.style.display = 'none'; 
+    socket.on('user.connected', function (userId) {
+        const connectionDiv = document.createElement('div');
+        connectionDiv.className = 'connection';
+        connectionDiv.textContent = userId + ' присоединился к чату';
+        document.getElementById('messages').appendChild(connectionDiv);
+    })
+
+    socket.on('user.disconnected', function (userId) {
+        const connectionDiv = document.createElement('div');
+        connectionDiv.className = 'connection';
+        connectionDiv.textContent = userId + ' вышел из чата';
+        document.getElementById('messages').appendChild(connectionDiv);
+    })
+
+    socket.on('user.id', function (id) {
+        userId = id;
+    });
+
+    modalBackground.style.display = 'none';
+}
+
+confirmUserIdButton.onclick = function () {
+    connect();
 };
 
-const sendMessage = function() {
-    const message = input.value;
-    socket.emit('chat.message', message);
-    input.value = '';
-};
-
-sendButton.onclick = sendMessage;
-
-input.onkeydown = function(event) {
+document.getElementById('user-id').onkeydown = function (event) {
     if (event.key === 'Enter') {
-        sendMessage();
+        connect();
     }
 };
-
-socket.on('user.id', function(id) {
-    userId = id;
-});
--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const io = require('socket.io')(server);
 
 app.use(express.static('public'));
 
-let userId;
-
 class Message {
     constructor(userId, text) {
         this.userId = userId;
@@ -20,18 +18,21 @@ class Filter {
         let filteredText = message.text;
         Filter.badWords.forEach(word => {
             const re = new RegExp(word, 'gi');
-            filteredText = filteredText.replace(re, (match) => {
-                const replacement = match[0] + '*'.repeat(match.length - 1);
-                return replacement;
-            });
+            try {
+                filteredText = filteredText.replace(re, (match) => {
+                    const replacement = match[0] + '*'.repeat(match.length - 1);
+                    return replacement;
+                });
+            } catch (error) {
+                console.log(error);
+            }
         });
-
         return new Message(message.userId, filteredText);
     }
 }
 
 const handler = {
-    set: function(target, property, value) {
+    set: function (target, property, value) {
         target[property] = Filter.apply(value);
         return true;
     }
@@ -41,16 +42,19 @@ io.on('connection', socket => {
     socket.on('user.connected', (userId) => {
         socket.userId = userId;
         console.log(userId + ' connected');
+        socket.broadcast.emit('user.connected', userId);
     });
-
     const chat = new Proxy({}, handler);
     socket.on('chat.message', message => {
         const messageInstance = new Message(socket.userId, message);
         chat.message = messageInstance;
         io.emit('chat.message', chat.message);
     });
+    socket.on('disconnect', () => {
+        console.log(socket.userId + ' disconnected');
+        socket.broadcast.emit('user.disconnected', socket.userId);
+    });
 });
-
 
 
 server.listen(3005, () => {
